da09830ea0f1ea4370fbdbdba0ce7925ae530411
tsai1247 <jeff29cc91079@gmail.com>
2021-03-25 23:37:12 +0800

    add some admin command

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # coding=UTF-8
+from logging import exception
 import random
-from warnings import catch_warnings
-from requests.models import parse_header_links
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 import requests
@@ -18,7 +17,15 @@ url = os.getenv("MD_SOURCE")
 status = {}
 add_query_classification = {}
 add_query_shopname = {}
+add_query_photolink = {}
 classMap = {'宵夜街':2, '後門':3, '奢侈接':4, '山下':5}
+
+def isDEVELOPER(chat_id):
+    developer_id = os.getenv("DEVELOPER_ID").split(',')
+    if chat_id in developer_id:
+        return True
+    else:
+        return False
 
 def updateHackmd(chat_id, classification, shopname, photolink):
     def GetReverseMenu(curMenu):
@@ -71,10 +78,14 @@ def updateHackmd(chat_id, classification, shopname, photolink):
         return ret
 
     def updatePhoto(code):
-        code += '### {}\n![]({} =400x)\n\n'.format(shopname, photolink)
+        code += '### {}\n'.format(shopname)
+        for i in photolink:
+            code += '![]({} =400x)\n'.format(i)
+        code += '\n'
         return code
     del(add_query_classification[chat_id])
-    del(add_query_shopname[chat_id])  
+    del(add_query_shopname[chat_id])
+    del(add_query_photolink[chat_id])
     code = split(getcode()) # len = 6
                             # 菜單 索引 宵夜街 後門 奢侈街 山下
     index = classMap[classification]
@@ -84,10 +95,21 @@ def updateHackmd(chat_id, classification, shopname, photolink):
     for i in code:
         newcode += i
     
+    fp = codecs.open("filename.txt", "r", "utf-8")
+    oldcode = fp.readlines
+    print(oldcode)
+    fp.close()
+
+    fp2 = codecs.open("filename_auto_back_up.txt", "w", "utf-8")
+    fp2.write(oldcode)
+    fp2.close()
+
     fp = codecs.open("filename.txt", "w", "utf-8")
     fp.write(newcode)
     fp.close()
     
+    
+
     command = "modules\\hackmd-overwriter\\bin\\overwrite.cmd " + os.getenv("MD_SOURCE") + ' filename.txt'
     os.system(command)
 
@@ -190,8 +212,6 @@ def help(update, bot):
         '/search : search a menu.\n'
         '/add : add new menu(not done).\n'
     )
-    # bot.send_photo(
-    #     chat_id=chat_id, photo='https://telegram.org/img/t_logo.png')
 
 def help_zh(update, bot):
    
@@ -202,6 +222,46 @@ def help_zh(update, bot):
         '/random : 隨機取得一個菜單。\n'
         '/search : 查詢菜單。\n'
         '/add : 新增菜單(未完成)。\n'
+    )
+
+
+def xhelp(update, bot):
+    chat_id = str(update.message.chat_id)
+    if(not isDEVELOPER(chat_id)):
+        update.message.reply_text(
+            "你沒有使用此指令的權限"
+        )
+        update.message.reply_text(
+            "但名豐可以請客"
+        )
+        return
+
+    update.message.reply_text(
+        'The followings are some commands: \n'
+        '/xhelpzh : 查看中文管理員指令說明\n'
+        '/xhelp : get this document.\n'
+        '/clear : (debug tool) reset all variable about chat id.\n'
+        '/backup : manually backup a hackmd data.\n'
+        '/undo : overwrite hackmd data with auto_backup data.\n'
+        '/restore : overwrite hackmd data with manual_backup data.\n'
+    )
+
+def xhelpzh(update, bot):
+    chat_id = str(update.message.chat_id)
+    if(not isDEVELOPER(chat_id)):
+        update.message.reply_text(
+            "你沒有使用此指令的權限"
+        )
+        return
+
+    update.message.reply_text(
+        '以下是管理員指令: \n'
+        '/xhelpzh : 查看此說明\n'
+        '/xhelp : get English document for administrator\'s commands\n'
+        '/clear : (除錯工具) 重置關於此 chat_id 的所有狀態\n'
+        '/backup : 手動備份資料。\n'
+        '/undo : 從上次的自動備份還原菜單。\n'
+        '/restore : 從上次的手動備份還原菜單。\n'
     )
 
 def randomfunc(update, bot):
@@ -237,16 +297,36 @@ def add(update, bot):
     # update.message.reply_text(
     #     '請輸入店家名稱'
     # )
-    update.message.reply_text("請選擇分類",
-        reply_markup = InlineKeyboardMarkup([[
-                InlineKeyboardButton(s, callback_data = '{} {}'.format(s, chat_id)) for s in ['宵夜街', '後門', '奢侈街', '山下']
-            ]]))
+    try:
+        if status[chat_id] == "add_step2":
+            del(status[chat_id])
+            update.message.reply_text(
+                    '正在新增店家...'
+                )
+            cur_classification = add_query_classification[chat_id]
+            cur_shopname = add_query_shopname[chat_id]
+            cur_photolink = add_query_photolink[chat_id]
+            
+            updateHackmd(chat_id, cur_classification, cur_shopname, cur_photolink)
+            update.message.reply_text(
+                '新增店家 {} 於分類 {}, 新增完成。'.format(cur_classification, cur_shopname)
+            )
+
+    except KeyError:
+        update.message.reply_text("請選擇分類",
+            reply_markup = InlineKeyboardMarkup([[
+                    InlineKeyboardButton(s, callback_data = '{} {}'.format(s, chat_id)) for s in ['宵夜街', '後門', '奢侈街', '山下']
+                ]]))
+
+   
 
     print('status:', status)
 
 def getClassification(update, bot):
     s, chat_id = update.callback_query.data.split(" ")
-    update.callback_query.edit_message_text('分類為：{}\n請輸入店家名稱'.format(s))
+    update.callback_query.edit_message_text(
+        '分類為：{}\n請輸入店家名稱'.format(s)
+    )
     status[chat_id] = "add_step1"
     add_query_classification[chat_id] = s
     print('status:', status)
@@ -304,50 +384,61 @@ def whengetphoto(update, bot):
     try:
         state = status[chat_id]
         if state == 'add_step2':
-            del(status[chat_id])
             update.message.reply_text(
                 '正在取得照片...'
             )
             photorequesturl = 'https://api.telegram.org/bot' + os.getenv("TELEGRAM_TOKEN") + '/getfile?file_id=' + update.message.photo[0].file_id
-            photolink = uploadAndGetPhoto(photorequesturl) 
+            photolink = uploadAndGetPhoto(photorequesturl)
+            if chat_id in add_query_photolink:
+                print("EXIST")
+                add_query_photolink[chat_id].append(photolink)
+            else:
+                print("NONE")
+                add_query_photolink[chat_id] = [photolink]
+                
             update.message.reply_text(
-                '正在新增店家...'
+                '請繼續傳送照片或輸入 /add 結束傳送'
             )
-            cur_classification = add_query_classification[chat_id]
-            cur_shopname = add_query_shopname[chat_id]
-            updateHackmd(chat_id, cur_classification, cur_shopname, photolink)
-            update.message.reply_text(
-                '新增店家 {} 於分類 {}, 新增完成。'.format(cur_classification, cur_shopname)
-            )
+
         print('status:', status)
     except KeyError:
         print('ignore it')
 
-def whengetfile(update, bot):  
+def whengetfile(update, bot):
     chat_id = str(update.message.chat_id)
     try:
         state = status[chat_id]
         if state == 'add_step2':
-            del(status[chat_id])
             update.message.reply_text(
                 '正在取得照片...'
             )
             photorequesturl = 'https://api.telegram.org/bot' + os.getenv("TELEGRAM_TOKEN") + '/getfile?file_id=' + update.message.document.file_id
-            photolink = uploadAndGetPhoto(photorequesturl) 
+            photolink = uploadAndGetPhoto(photorequesturl)
+            if chat_id in add_query_photolink:
+                print("EXIST")
+                add_query_photolink[chat_id].append(photolink)
+            else:
+                print("NONE")
+                add_query_photolink[chat_id] = [photolink]
+                
             update.message.reply_text(
-                '正在新增店家...'
+                '請繼續傳送照片或輸入 /add 結束傳送'
             )
-            cur_classification = add_query_classification[chat_id]
-            cur_shopname = add_query_shopname[chat_id]
-            updateHackmd(chat_id, cur_classification, cur_shopname, photolink)
-            update.message.reply_text(
-                '新增店家 {} 於分類 {}, 新增完成。'.format(cur_classification, cur_shopname)
-            )
+
         print('status:', status)
     except KeyError:
         print('ignore it')
+
+
+
 def clearallrequest(update, bot):
     chat_id = str(update.message.chat_id)
+    if(not isDEVELOPER(chat_id)):
+        update.message.reply_text(
+            "你沒有使用此指令的權限"
+        )
+        return
+
     try:
         del(status[chat_id])
     except KeyError:
@@ -360,10 +451,75 @@ def clearallrequest(update, bot):
         del(add_query_classification[chat_id])
     except KeyError:
         print('ignore it')
+    try:
+        del(add_query_photolink[chat_id])
+    except KeyError:
+        print('ignore it')
+    
     update.message.reply_text(
         "已清除您的所有要求"
     )
 
+def backup(update, bot):
+    chat_id = str(update.message.chat_id)
+    if(not isDEVELOPER(chat_id)):
+        update.message.reply_text(
+            "你沒有使用此指令的權限"
+        )
+        return
+
+    fp = codecs.open("filename.txt", "r", "utf-8")
+    oldcode = fp.readlines()
+    fp.close()
+    
+    fp2 = codecs.open("filename_back_up.txt", "w", "utf-8")
+    for i in oldcode:
+        fp2.write(i)
+    fp2.close()
+
+    update.message.reply_text(
+        "手動備份完成"
+    )
+
+def undo(update, bot):
+    chat_id = str(update.message.chat_id)
+    if(not isDEVELOPER(chat_id)):
+        update.message.reply_text(
+            "你沒有使用此指令的權限"
+        )
+        return
+
+    update.message.reply_text(
+        "還原中..."
+    )
+    
+    command = "modules\\hackmd-overwriter\\bin\\overwrite.cmd " + os.getenv("MD_SOURCE") + ' filename_auto_back_up.txt'
+    os.system(command)
+
+    update.message.reply_text(
+        "以自動備份檔還原完成"
+    )
+
+def restore(update, bot):
+    chat_id = str(update.message.chat_id)
+    if(not isDEVELOPER(chat_id)):
+        update.message.reply_text(
+            "你沒有使用此指令的權限"
+        )
+        return
+
+    update.message.reply_text(
+        "還原中..."
+    )
+
+    command = "modules\\hackmd-overwriter\\bin\\overwrite.cmd " + os.getenv("MD_SOURCE") + ' filename_back_up.txt'
+    os.system(command)
+
+    update.message.reply_text(
+        "以手動備份檔還原完成"
+    )
+        
+        
 
 # Main
 def main():
@@ -376,11 +532,23 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('random', randomfunc))
     updater.dispatcher.add_handler(CommandHandler('add', add))
     updater.dispatcher.add_handler(CommandHandler('search', search))
+    
+    # debugging tools
+    updater.dispatcher.add_handler(CommandHandler('xhelp', xhelp))
+    updater.dispatcher.add_handler(CommandHandler('xhelpzh', xhelpzh))
     updater.dispatcher.add_handler(CommandHandler('clear', clearallrequest))
+    updater.dispatcher.add_handler(CommandHandler('backup', backup))
+    updater.dispatcher.add_handler(CommandHandler('undo', undo))
+    updater.dispatcher.add_handler(CommandHandler('restore', restore))
+
+    # normal messages
     updater.dispatcher.add_handler(MessageHandler(Filters.text, filtermsg))
     updater.dispatcher.add_handler(MessageHandler(Filters.photo, whengetphoto))
     updater.dispatcher.add_handler(MessageHandler(Filters.document, whengetfile))
+
+    # calbackhandler
     updater.dispatcher.add_handler(CallbackQueryHandler(getClassification))
+
 
 
     updater.start_polling()
