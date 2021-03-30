abe02bb772208c41a24d9cb6e41b6d0efcf1baba
tsai1247 <jeff29cc91079@gmail.com>
2021-03-30 19:02:41 +0800

    logger paras bug fixed

--- overwrite.py
+++ overwrite.py
@@ -3,8 +3,10 @@ import platform
 
 def overwrite(srcdata):
     print('overwritting,', platform.system())
+    command = ''
     if platform.system() == 'Windows':
         command = "modules\\hackmd-overwriter\\bin\\overwrite.cmd " + os.getenv("MD_SOURCE") + ' ' + srcdata
     else:
         command = "modules/hackmd-overwriter/bin/overwrite " + os.getenv("MD_SOURCE") + ' ' + srcdata
+    print(command)
     os.system(command)
