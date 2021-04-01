2a9f78ce34fe4c1826a126b86f49bec590fefe8b
tsai1247 <jeff29cc91079@gmail.com>
2021-04-02 00:45:36 +0800

    cleaner code (except interact_with_hackmd.py)

--- interact_with_hackmd.py
+++ interact_with_hackmd.py
@@ -14,7 +14,7 @@ def Levenshtein(str1, str2):
     if str1==str2:
         return 0
     
-    cost = 1 if str1[0] == str2[0] else 0
+    cost = 0 if str1[0] == str2[0] else 1
 
     a = Levenshtein(str1[1:str1_len], str2) + 1
     if a>5: return 5
